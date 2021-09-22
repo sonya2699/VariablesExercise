a4f99d9013886f72c5247124262d4286f720f8c4
Student <student@Cumberlandss-MacBook-Pro.local>
2021-09-22 13:12:31 -0500

    Finished the VariablesExercise with the Bonus

---
+++ script.js
@@ -0,0 +1,42 @@
+// VariablesExercise
+
+// #1
+// The Constant/const is the variable type used for a variable whose value will not change. 
+
+// #2
+let num = 20;
+
+// #3
+num = 15;
+
+// #4
+num = num + 7;
+
+// #5
+num--;
+
+// #6
+num = "This is not a number";
+
+// #7
+let blank;
+
+// #8
+console.log(blank);
+
+// #9
+blank = null;
+
+// Bonus
+
+// #1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+
+// #2
+console.log(quarter, dime, nickel, penny);
+
+// #3
+let letters = "qwertyuiop";
+
+// #4
+console.log(letters[letters.length - 1]);
